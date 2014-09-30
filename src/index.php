b78584266f7e9ead50c176eb06dec56fc5c9dc21
Rogers Guedes <rogerguedes.ft@gmail.com>
2014-09-30 15:40:32 -0300

    Configurado para procurar por comentarios em arquivos '.md' e '.php' dentro das pastas 'src' e doxygenConfigFiles

---
+++ src/index.php
@@ -0,0 +1,13 @@
+/**
+ * This is my test class
+ */
+
+<?php
+    class TestClass{
+        public $attr1 = 1;/** < this is a fucking atribute*/
+        public function getAttr1(){ /** this is a ordinary method*/
+            return $this->attr1;
+        }
+    }
+?>
+
